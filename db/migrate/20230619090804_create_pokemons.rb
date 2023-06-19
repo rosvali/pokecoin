6997266ac0df4072416b5198b80e1daaa8fe9336
Rosalie Aguillon <rosval6578@gmail.com>
2023-06-19 11:25:35 +0200

    pokemon migration

---
+++ db/migrate/20230619090804_create_pokemons.rb
@@ -0,0 +1,13 @@
+class CreatePokemons < ActiveRecord::Migration[7.0]
+  def change
+    create_table :pokemons do |t|
+      t.string :name
+      t.integer :weight
+      t.integer :height
+      t.integer :price
+      t.belongs_to :user, index: true
+
+      t.timestamps
+    end
+  end
+end
