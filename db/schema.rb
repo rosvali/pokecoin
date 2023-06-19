eafdb8e3752b090f9a38f5b39f65c5dba8fc6b68
Rosalie Aguillon <rosval6578@gmail.com>
2023-06-19 11:23:23 +0200

    add pokemon model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_06_19_090006) do
+ActiveRecord::Schema[7.0].define(version: 2023_06_19_090804) do
+  create_table "pokemons", force: :cascade do |t|
+    t.string "name"
+    t.integer "weight"
+    t.integer "height"
+    t.integer "price"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_pokemons_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
