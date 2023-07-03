235ce978b6fbcaa9adf75e5f67f17ee8a8eec0bf
rosvali <rosval6578@gmail.com>
2023-07-03 14:54:18 +0200

    add registration and session with devise

--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,9 @@ Rails.application.routes.draw do
   get 'pokemon/:id/checkout', to: 'pokemons#checkout', as: :pokemon_checkout
   post 'pokemon/:id/buy', to: 'pokemons#buy', as: :pokemon_buy
   post 'pokemon/:id/sell', to: 'pokemons#sell', as: :pokemon_sell
-  get 'me', to: 'users#show', as: :user
-  get 'me/add', to: 'users#edit', as: :user_edit
-  patch 'me/add', to: 'users#update', as: :user_update
-  get 'me/transactions', to: 'users#transactions', as: :user_transactions
+  # get 'me', to: 'users#show', as: :user
+  # get 'me/add', to: 'users#edit', as: :user_edit
+  # patch 'me/add', to: 'users#update', as: :user_update
+  # get 'me/transactions', to: 'users#transactions', as: :user_transactions
+  devise_for :users
 end
