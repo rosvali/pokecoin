d99c45c7b195ac86b26f98cec74f27a0f76191d9
rosvali <rosval6578@gmail.com>
2023-07-03 15:57:22 +0200

    comment controllers method to do

--- app/controllers/pokemons_controller.rb
+++ app/controllers/pokemons_controller.rb
@@ -1,4 +1,6 @@
 class PokemonsController < ApplicationController
+  before_action :authenticate_user!, except: [:index]
+
   def index
     pokemons = Pokemon.all
     render json: pokemons, status: 200
@@ -9,25 +11,25 @@ class PokemonsController < ApplicationController
     @user = @pokemon.user
   end
 
-  def checkout
-    pokemon = pokemon_finder
-    user = pokemon.user
-    price = pokemon.price
-    balance = current_user.balance
-    render json: { pokemon: pokemon, vendeur: user, prix: price, balance: balance }, status: 200
-  end
+  # def checkout
+  #   pokemon = pokemon_finder
+  #   user = pokemon.user
+  #   price = pokemon.price
+  #   balance = current_user.balance
+  #   render json: { pokemon: pokemon, vendeur: user, prix: price, balance: balance }, status: 200
+  # end
 
-  def buy
-    pokemon = pokemon_finder
-    vendeur = pokemon_finder.user
-    if current_user.balance >= pokemon.price && current_user != vendeur
-      Transaction.create(pokemon: pokemon, user: current_user, action: 0)
-      Transaction.create(pokemon: pokemon, user: vendeur, action: 1)
-      render json: pokemon, status: 200
-    else
-      render json: {message: "no"}, status: 500
-    end
-  end
+  # def buy
+  #   pokemon = pokemon_finder
+  #   vendeur = pokemon_finder.user
+  #   if current_user.balance >= pokemon.price && current_user != vendeur
+  #     Transaction.create(pokemon: pokemon, user: current_user, action: 0)
+  #     Transaction.create(pokemon: pokemon, user: vendeur, action: 1)
+  #     render json: pokemon, status: 200
+  #   else
+  #     render json: {message: "no"}, status: 500
+  #   end
+  # end
 
   private
 
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,19 +1,21 @@
 class UsersController < ApplicationController
-    def show
-        render json: current_user, status: 200
-    end
+    before_action :authenticate_user!
+    
+    # def show
+    #     render json: current_user, status: 200
+    # end
 
-    def edit
-        render json: { wallet: current_user.balance}, status: 200
-    end
+    # def edit
+    #     render json: { wallet: current_user.balance}, status: 200
+    # end
 
-    def update
-        new_balance = current_user.balance + 50
-        current_user.update(balance: new_balance)
-        render json: current_user, status: 200
-    end
+    # def update
+    #     new_balance = current_user.balance + 50
+    #     current_user.update(balance: new_balance)
+    #     render json: current_user, status: 200
+    # end
 
-    def transactions
-        render json: current_user.transactions, status: 200
-    end
+    # def transactions
+    #     render json: current_user.transactions, status: 200
+    # end
 end
