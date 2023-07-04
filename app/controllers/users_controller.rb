35cefb3c42b9973249811addd7409ee5b4cbc12e
rosvali <rosval6578@gmail.com>
2023-07-04 14:53:24 +0200

    add balance with button

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,11 +9,14 @@ class UsersController < ApplicationController
     #     render json: { wallet: current_user.balance}, status: 200
     # end
 
-    # def update
-    #     new_balance = current_user.balance + 50
-    #     current_user.update(balance: new_balance)
-    #     render json: current_user, status: 200
-    # end
+    def update
+        new_balance = current_user.balance + 50
+        current_user.update(balance: new_balance)
+        respond_to do |format|
+			format.html { redirect_to root_path }
+			format.js {}
+		end
+    end
 
     # def transactions
     #     render json: current_user.transactions, status: 200
