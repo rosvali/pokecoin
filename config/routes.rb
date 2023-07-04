a9f0f78e4ad0024ee3e741c8cb51e043cb24d798
rosvali <rosval6578@gmail.com>
2023-07-04 13:59:55 +0200

    setup show pokemon

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,9 @@
 Rails.application.routes.draw do
   root 'pages#home'
-  resources :pokemons, only: [:index]
-  get 'pokemon/:id', to: 'pokemons#show', as: :pokemon
-  get 'pokemon/:id/checkout', to: 'pokemons#checkout', as: :pokemon_checkout
-  post 'pokemon/:id/buy', to: 'pokemons#buy', as: :pokemon_buy
-  post 'pokemon/:id/sell', to: 'pokemons#sell', as: :pokemon_sell
+  resources :pokemons, only: [:index, :show]
+  # get 'pokemon/:id/checkout', to: 'pokemons#checkout', as: :pokemon_checkout
+  # post 'pokemon/:id/buy', to: 'pokemons#buy', as: :pokemon_buy
+  # post 'pokemon/:id/sell', to: 'pokemons#sell', as: :pokemon_sell
   # get 'me', to: 'users#show', as: :user
   # get 'me/add', to: 'users#edit', as: :user_edit
   # patch 'me/add', to: 'users#update', as: :user_update
