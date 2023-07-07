ceedf56965070fc6172c03925fd048bb14465b8e
Rosalie Aguillon <rosval6578@gmail.com>
2023-07-07 13:19:25 +0200

    add tests for transaction model

--- test/models/transaction_test.rb
+++ test/models/transaction_test.rb
@@ -1,7 +1,54 @@
 require "test_helper"
 
 class TransactionTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "valid transaction" do
+    user = users(:user1)
+    pokemon = pokemons(:pokemon1)
+    transaction = Transaction.create(action: 0, user: user, pokemon: pokemon)
+    
+    assert transaction.valid?
+  end
+
+  test "transaction invalid user missing" do
+    pokemon = pokemons(:pokemon1)
+    transaction = Transaction.create(action: 0, pokemon: pokemon)
+    
+    refute transaction.valid?
+  end
+
+  test "transaction invalid pokemon missing" do
+    user = users(:user1)
+    transaction = Transaction.create(action: 0, user: user)
+    
+    refute transaction.valid?
+  end
+
+  test "transaction user and pokemon affiliation" do
+    user = users(:user1)
+    pokemon = pokemons(:pokemon1)
+    transaction = Transaction.create(action: 0, user: user, pokemon: pokemon)
+    
+    assert_equal transaction.user.id, user.id
+    assert_equal transaction.pokemon.id, pokemon.id
+  end
+
+  test "transaction action" do
+    user = users(:user1)
+    pokemon = pokemons(:pokemon1)
+    transaction1 = Transaction.create(action: 0, user: user, pokemon: pokemon)
+    transaction2 = Transaction.create(action: 1, user: user, pokemon: pokemon)
+    
+    assert_equal transaction1.action, "buy"
+    assert_equal transaction2.action, "sell"
+  end
+
+  test "transaction modif method should return Achat or Vente" do
+    user = users(:user1)
+    pokemon = pokemons(:pokemon1)
+    transaction1 = Transaction.create(action: 0, user: user, pokemon: pokemon)
+    transaction2 = Transaction.create(action: 1, user: user, pokemon: pokemon)
+    
+    assert_equal transaction1.motif, "Achat"
+    assert_equal transaction2.motif, "Vente"
+  end
 end
