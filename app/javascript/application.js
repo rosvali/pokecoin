8aebd88b107c07a0006ba4016725d21cec1809ee
rosvali <rosval6578@gmail.com>
2023-07-20 15:22:17 +0200

    setup vuejs

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,14 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
+//= require vue
+//= require vue-router (optional)
+//= require vue-resource (optional)
+//= require vuex (optional)
 import "@hotwired/turbo-rails"
 import "controllers"
+
+new Vue({
+    el: '#app',
+    components: {
+      'my-component': mycomponent
+    }
+  })
